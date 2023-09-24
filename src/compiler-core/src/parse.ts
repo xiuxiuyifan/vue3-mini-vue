3ab98526243c3fc03784fb07bd8f5d00a4197de9
razl92@163.com <8791173qq>
2023-09-24 15:47:24 +0800

    实现三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -12,37 +12,78 @@ const enum TagType {
 export function baseParse(content: string) {
   const context = createParserContext(content);
 
-  return createRoot(parseChildren(context));
+  return createRoot(parseChildren(context, []));
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
   const nodes: any = [];
 
-  let node;
-  const s = context.source;
-  // 解析插值类型
-  if (s.startsWith("{{")) {
-    // 将 context 交给专门处理插值的函数进行处理
-    node = parseInterpolation(context);
-  }
-  // 元素类型  字符串的开头是 < 开始的
-  else if (s[0] === "<") {
-    // 再看第一个元素是不是 a-z
-    if (/[a-z]/i.test(s[1])) {
-      node = parseElement(context);
+  // 什么时候停止这个循环呢？
+  // 没有结束的时候就一直进行循环
+  while (!isEnd(context, ancestors)) {
+    let node;
+    const s = context.source;
+    // 解析插值类型
+    if (s.startsWith("{{")) {
+      // 将 context 交给专门处理插值的函数进行处理
+      node = parseInterpolation(context);
     }
-  }
+    // 元素类型  字符串的开头是 < 开始的
+    else if (s[0] === "<") {
+      // 再看第一个元素是不是 a-z
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors);
+      }
+    }
 
-  if (!node) {
-    node = parseText(context);
+    if (!node) {
+      node = parseText(context);
+    }
+    nodes.push(node);
   }
-  nodes.push(node);
   return nodes;
 }
 
+function isEnd(context, ancestors) {
+  // 1. source 有值的时候
+  // 2. 遇到结束标签的时候
+  const s = context.source;
+
+  // 如果是一个结束标签的话
+  if (s.startsWith("</")) {
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      const tag = ancestors[i].tag;
+      // </div>
+      if (startsWithEndTagOpen(s, tag)) {
+        return true;
+      }
+    }
+  }
+
+  // if (s && s.startsWith(`</${parentTag}>`)) {
+  //   return true;
+  // }
+
+  return !s;
+}
+
+// 之前是把整个字符串的长度作为结束的位置
 function parseText(context: any) {
+  let endIndex = context.source.length;
+  let endTokens = ["<", "{{"];
+
+  // 看文本里面，有没有插值
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i]);
+    // 如果有插值的话，则截取到插值的开头位置就好了。
+    if (index !== -1 && endIndex > index) {
+      // 取最靠近左边的结束标签。最左边的赋值完，后面的就进不来了
+      endIndex = index;
+    }
+  }
+
   // 获取 content 内容
-  const content = parseTextData(context, context.source.length);
+  const content = parseTextData(context, endIndex);
 
   return {
     type: NodeTypes.TEXT,
@@ -61,13 +102,33 @@ function parseTextData(context: any, length) {
 }
 
 // 解析 元素
-function parseElement(context: any) {
+function parseElement(context: any, ancestors) {
   // 先处理元素的开始标签
-  const element = parseTag(context, TagType.Start);
+  const element: any = parseTag(context, TagType.Start);
+  // 收集解析的 元素标签到栈里面
+  ancestors.push(element);
+  // 处理元素的孩子
+  element.children = parseChildren(context, ancestors);
+  ancestors.pop();
   // 在处理 元素的结束标签
-  parseTag(context, TagType.End);
+
+  // 当前正在处理的标签 和剩下的标签进行一个匹配
+  // 如果匹配上了，则进行消费，
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag(context, TagType.End);
+  } else {
+    throw new Error(`缺少结束标签:${element.tag}`);
+  }
 
   return element;
+}
+
+function startsWithEndTagOpen(source, tag) {
+  return (
+    // 以 结束标签开始
+    source.startsWith("</") &&
+    source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+  );
 }
 
 function parseTag(context: any, type: TagType) {
